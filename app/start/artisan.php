6697946854ba5f389a60c3f4d62de18fe75cba9b
natsu90 <solomon_scorpio90@yahoo.com>
2015-03-12 23:43:41 +0800

    update composer and rm env-dot-gen

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -10,6 +10,5 @@
 | the console gets access to each of the command object instances.
 |
 */
-Artisan::add(new mathiasgrimm\laraveldotenvgen\EnvGen());
 Artisan::add(new sendTextCommand);
 Artisan::add(new setupNexmoCommand);
